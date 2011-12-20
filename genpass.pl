f0a4f58fa71e6326b5fcda7ae80827695e1e4528
Scusi <scusi@snurn.de>
2011-12-20 20:53:39 +0100

    genpass.pl extended
    added more types of passwords that 'genpass.pl' can generate.

--- genpass.pl
+++ genpass.pl
@@ -8,18 +8,27 @@ use Getopt::Std;
 
 $Getopt::Std::STANDARD_HELP_VERSION = "1";
 
-getopt('hl:n:');
-our ($opt_l,$opt_n);
+getopt('hl:n:t:');
+our ($opt_l,$opt_n,$opt_t);
 
 # Password length
-my $pwd_len = $opt_l || "12";
+my $pwd_len = int $opt_l || "12";
 
 # number of passwords
-my $num_pwds = $opt_n || "3";
+my $num_pwds = int $opt_n || "3";
 my $gen_pwds = "0";
 
+# type of password (alpha, num, alphanum, special, hex)
+my $pwd_type = $opt_t || "alphanum";
+
+# Standard Messages
 sub VERSION_MESSAGE {print "Version 0.1 alpha";}
-sub HELP_MESSAGE {print "Don't Panic!";}
+sub HELP_MESSAGE {
+    print "USAGE: $0 [-l 12] [-n 3] [-t alphanum]\n";
+    print ("-l    length of password to generate\n");
+    print ("-n    number of passwords to generate\n");
+    print ("-t    type of passwords to generate, can be alpha, num, alphanum, hex, special\n");
+}
 
 while ($gen_pwds < $num_pwds) {
     &gen_passwd();
@@ -27,7 +36,20 @@ while ($gen_pwds < $num_pwds) {
 }
 
 sub gen_passwd {
-    my @chars = ( "A" .. "Z", "a" .. "z", 0 .. 9, qw(+ - _ ? = ! @ $ % ^ & * :) );
+    my @chars = ( "A" .. "Z", "a" .. "z", 0 .. 9 );
+    if ($pwd_type eq "num") {
+        @chars = ( 0 .. 9 );
+    } elsif ($pwd_type eq "alpha") {
+        @chars = ( "A" .. "Z", "a" .. "z" );
+    } elsif ($pwd_type eq "alphanum") {
+        @chars = ( "A" .. "Z", "a" .. "z", 0 .. 9 );
+    } elsif ($pwd_type eq "special") {
+        @chars = ( "A" .. "Z", "a" .. "z", 0 .. 9, qw(+ - _ ? = ! @ $ % ^ & * :) );
+    } elsif ($pwd_type eq "hex") {
+        @chars = ( "A" .. "F", 0 .. 9 );
+    } else {
+        die ("Check your password type! No supported type found!\n");
+    }
     my $password = join("", @chars[ map { rand @chars } ( 1 .. $pwd_len ) ]);
     print ("$password\n");
 }
