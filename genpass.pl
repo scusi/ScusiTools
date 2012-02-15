8e07235731cd3a170c46260b49210572796222d7
Scusi <scusi@snurn.de>
2012-02-15 11:32:08 +0100

    Added 'easy' Mode in genpass.pl and some minor bug fixes.

--- genpass.pl
+++ genpass.pl
@@ -6,16 +6,18 @@
 use strict;
 use Getopt::Std;
 
-$Getopt::Std::STANDARD_HELP_VERSION = "1";
+$Getopt::Std::STANDARD_HELP_VERSION = "true";
 
 getopt('hl:n:t:');
-our ($opt_l,$opt_n,$opt_t);
+our ($opt_h,$opt_v,$opt_l,$opt_n,$opt_t);
 
 # Password length
-my $pwd_len = int $opt_l || "12";
+my $pwd_len = $opt_l || "12";
+$pwd_len = int($pwd_len);
 
 # number of passwords
-my $num_pwds = int $opt_n || "3";
+my $num_pwds = $opt_n || "3";
+$num_pwds = int($num_pwds);
 my $gen_pwds = "0";
 
 # type of password (alpha, num, alphanum, special, hex)
@@ -27,7 +29,11 @@ sub HELP_MESSAGE {
     print "USAGE: $0 [-l 12] [-n 3] [-t alphanum]\n";
     print ("-l    length of password to generate\n");
     print ("-n    number of passwords to generate\n");
-    print ("-t    type of passwords to generate, can be alpha, num, alphanum, hex, special\n");
+    print ("-t    type of passwords to generate, can be easy, alpha, num, alphanum, hex, special\n");
+}
+if ($opt_h) {
+    HELP_MESSAGE();
+    exit;
 }
 
 while ($gen_pwds < $num_pwds) {
@@ -47,9 +53,20 @@ sub gen_passwd {
         @chars = ( "A" .. "Z", "a" .. "z", 0 .. 9, qw(+ - _ ? = ! @ $ % ^ & * :) );
     } elsif ($pwd_type eq "hex") {
         @chars = ( "A" .. "F", 0 .. 9 );
+    } elsif ($pwd_type eq "easy") {
+        @chars = ("A" .. "F", 0 .. 9, "a" .. "z", 0 .. 9);
     } else {
         die ("Check your password type! No supported type found!\n");
     }
-    my $password = join("", @chars[ map { rand @chars } ( 1 .. $pwd_len ) ]);
+    my $password = "";
+    if ($pwd_type eq "easy") {
+        my $part1 = join("", @chars[map { rand @chars } (1 .. 4) ]);
+        my $part2 = join("", @chars[map { rand @chars } (1 .. 4) ]);
+        my $part3 = join("", @chars[map { rand @chars } (1 .. 4) ]);
+        my $part4 = join("", @chars[map { rand @chars } (1 .. 4) ]);
+        $password = join("-", $part1, $part2, $part3, $part4);
+    } else {
+        $password = join("", @chars[ map { rand @chars } ( 1 .. $pwd_len ) ]);
+    }
     print ("$password\n");
 }
