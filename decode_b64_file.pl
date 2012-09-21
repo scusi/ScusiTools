d841c80c1271f48f9b1c11946867c9cda2d9d1ce
Scusi <scusi@snurn.de>
2012-09-21 16:12:21 +0200

    small modification in 'decode_b64.pl'

--- decode_b64_file.pl
+++ decode_b64_file.pl
@@ -1,6 +1,11 @@
 #!/usr/bin/perl -w
 #
 # script to decode base64 files
+#
+# use like this: 
+#  ./decode_b64_file.pl myfile.b64 > myfile.bin
+#  ./decode_b64_file.pl myfile.b64 | hexdump -c
+#
 # scusi@snurn.de
 # 15.09.2010
 use strict;
